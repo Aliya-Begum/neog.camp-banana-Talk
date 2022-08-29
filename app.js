3fc1d0b9416523fcf43eb8163da15682b4f14a6d
Aliya-Begum <111738881+Aliya-Begum@users.noreply.github.com>
2022-08-29 21:11:48 +0530

    input_output

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var btnInput = document.querySelector(".txt-input");
+var outputDiv = document.querySelector(".output-div") ;
 
 console.log(btnInput);
 
 function clickHandler(){
-    console.log("clicked");
-    console.log("input",btnInput.value);
+    outputDiv.innerText = "euhhdij" +btnInput.value; 
 }
 
 btnTranslate.addEventListener("click", clickHandler);
-var txtArea = document.querySelector("textarea");
-var btnPrimary = document.querySelector('.btn-primary');
-var btnInput = document.querySelector('#input-btn');
-var translator = document.querySelector("[name='translator']");
-console.log(txtArea);
-console.log(btnPrimary);
-console.log(btnInput);
-console.log(translator);
